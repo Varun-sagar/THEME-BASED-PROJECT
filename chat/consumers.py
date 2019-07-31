736381f5130917fd1c4cda5bc612139e6f059837
Varun-sagar <44369340+Varun-sagar@users.noreply.github.com>
2019-07-31 18:04:04 +0530

    Update consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -12,14 +12,7 @@ import json
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
-    """
-    This chat consumer handles websocket connections for chat clients.
-
-    It uses AsyncJsonWebsocketConsumer, which means all the handling functions
-    must be async functions, and any sync work (like ORM access) has to be
-    behind database_sync_to_async or sync_to_async. For more, read
-    http://channels.readthedocs.io/en/latest/topics/consumers.html
-    """
+   
     #@database_sync_to_async
     async def fetch_message(self,room):
         print("fetch man!")
@@ -41,9 +34,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
     ##### WebSocket event handlers
 
     async def connect(self):
-        """
-        Called when the websocket is handshaking as part of initial connection.
-        """
+        
         # Are they logged in?
 
         if self.scope["user"].is_anonymous:
@@ -67,10 +58,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def receive_json(self, content):
-        """
-        Called when we get a text frame. Channels will JSON-decode the payload
-        for us and pass it as the first argument.
-        """
+       
         # Messages will have a "command" key we can switch on
         command = content.get("command", None)
         print(content)
@@ -103,9 +91,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def disconnect(self, code):
-        """
-        Called when the WebSocket closes for any reason.
-        """
+       
         # Leave all the rooms we are still in
         print("closed man!!")
         for room_id in list(self.rooms):
@@ -118,9 +104,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def join_room(self, room_id):
-        """
-        Called by receive_json when someone sent a join command.
-        """
+       
         # The logged-in user is in our scope thanks to the authentication ASGI middleware
         room = await get_room_or_error(room_id, self.scope["user"])
         # Send a join message if it's turned on
@@ -153,9 +137,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def leave_room(self, room_id):
-        """
-        Called by receive_json when someone sent a leave command.
-        """
+       
         # The logged-in user is in our scope thanks to the authentication ASGI middleware
         room = await get_room_or_error(room_id, self.scope["user"])
         # Send a leave message if it's turned on
@@ -182,9 +164,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
 
 
     async def send_room(self, room_id, message,author=None,command=None):
-        """
-        Called by receive_json when someone sends a message to a room.
-        """
+        
         # Check they are in this room
 
         print(self.rooms)
